767878fb6d279087693bb3f58a31ab1283da164a
KellamA <kellamadams@gmail.com>
2024-09-16 14:38:31 -0700

    test code
    first push

---
+++ DineAuto/DineAuto.cpp
@@ -0,0 +1,12 @@
+﻿// DineAuto.cpp : Defines the entry point for the application.
+//
+
+#include "DineAuto.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
