f1eaf7c025312342f73b866607a538a82854020c
Shian <shianwen123@gmail.com>
2024-10-20 23:36:03 -0700

    added addToCart() and displayCart() functions

--- ShianBuild/CMakeFiles/DineAutoProgram.dir/DependInfo.cmake
+++ ShianBuild/CMakeFiles/DineAutoProgram.dir/DependInfo.cmake
@@ -9,7 +9,9 @@ set(CMAKE_DEPENDS_LANGUAGES
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "/home/shianwen123/DineAuto/322DineAutoProject/main.cpp" "CMakeFiles/DineAutoProgram.dir/main.cpp.o" "gcc" "CMakeFiles/DineAutoProgram.dir/main.cpp.o.d"
+  "/home/shianwen123/DineAuto/322DineAutoProject/menuItem.cpp" "CMakeFiles/DineAutoProgram.dir/menuItem.cpp.o" "gcc" "CMakeFiles/DineAutoProgram.dir/menuItem.cpp.o.d"
   "/home/shianwen123/DineAuto/322DineAutoProject/orderDataBase.cpp" "CMakeFiles/DineAutoProgram.dir/orderDataBase.cpp.o" "gcc" "CMakeFiles/DineAutoProgram.dir/orderDataBase.cpp.o.d"
+  "/home/shianwen123/DineAuto/322DineAutoProject/shoppingCart.cpp" "CMakeFiles/DineAutoProgram.dir/shoppingCart.cpp.o" "gcc" "CMakeFiles/DineAutoProgram.dir/shoppingCart.cpp.o.d"
   )
 
 # Targets to which this target links.
